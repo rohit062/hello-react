4c321e14c8f20a36b927f298f7b38ce6bbfa2895
Rohit Salaria <rohit.salaria@jungleworks.com>
2020-04-29 23:55:09 +0530

    feat: blog comment ui

--- src/CommentDetails.js
+++ src/CommentDetails.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import faker from 'faker'
 
 const CommentDetails = () => {
   return (
-    <div>
-      Hi
+    <div className='ui container comments'>
+      <div className='comment'>
+        <a href='/'  className='avatar'>
+          <img alt='avatar'  src={faker.image.avatar()} />
+        </a>
+      </div>
+      <div className='content' >
+        <a href='/' className="acthor">
+          Sam
+        </a>
+      </div>
+      <div className='metadata' >
+        <span href='/' className="date">
+          Today at 5 PM
+        </span>
+      </div>
+      <div className='text'> Nice</div>
     </div>
   )
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import ReactDom from 'react-dom';
 import CommentDetails from './CommentDetails';
 // create a react component
 const App = () => {
-  return <CommentDetails/>
+  return (
+    <div>
+        <CommentDetails/>
+        <CommentDetails/>
+        <CommentDetails/>
+        <CommentDetails/>
+    </div>
+    )
 }
 
 // Take the react  component and show it on the screen
